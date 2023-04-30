f4f390405bafd5f5786316bfc18de7a3f1496c04
VasiliiSokolov <vasilysokolov@mail.ru>
2023-04-30 18:59:36 +0300

    Done!

--- Player.cpp
+++ Player.cpp
@@ -1,69 +1,14 @@
-﻿#include <iostream>
+﻿#pragma warning(disable : 4996)
+#include <iostream>
 #include <ctime>
 #include <iomanip>
 #include <cstdlib>
 
-class Track;
-class Player;
-
-enum operation
-{
-	play = 0,
-	pause = 1,
-	next = 2,
-	stop = 3,
-	ex = 4
-};
-
-int main()
-{
-	int op;
-	std::cout << "Audio Player." << std::endl;
-	Player* player = new Player();
-	std::cout << "Loading tracks: " << std::endl;
-	player->setList();
-	while (true)
-	{
-		std::cout << "Choose the operation: ";
-		std::cin >> op;
-		if (op > 4 || op < 0)
-		{
-			std::cerr << "Error! Wrong operation! Try again!" << std::endl;
-		}
-		else
-		{
-			switch (op)
-			{
-			case operation::play:
-				player->pl();
-				break;
-			case operation::pause:
-				player->pa();
-				break;
-			case operation::next:
-				player->ne();
-				break;
-			case operation::stop:
-				player->st();
-				break;
-			case operation::ex:
-				delete player;
-				player = nullptr;
-				return 0;
-
-			}
-		}
-
-
-	}
-
-
-}
-
 class Track
 {
-	std::string name;
-	std::tm date;
+	std::string name = " ";
+	std::time_t t = std::time(nullptr);
+	std::tm date = *std::localtime(&t);
 	int trackDuration = 0;
 	/*std::cin >> std::get_time(&in.date, "%d/%m");*/
 
@@ -73,9 +18,9 @@ public:
 	{
 		return name;
 	}
-	std::tm getDate()
+	void getDate()
 	{
-		return date;
+		std::cout << std::put_time(&date, "%d/%m/%y") << std::endl;
 	}
 	int getDuration()
 	{
@@ -93,12 +38,12 @@ public:
 	{
 		trackDuration = newDuration;
 	}
-	
+
 };
 
 class Player
 {
-	Track playList[8];
+	Track playList[8]{};
 	bool play = false;
 	bool pause = false;
 	Track current;
@@ -106,7 +51,7 @@ class Player
 public:
 	void setList()
 	{
-		
+
 		for (int i = 0; i < 8; i++)
 		{
 			std::string name;
@@ -122,7 +67,7 @@ public:
 			std::cout << "Enter Track's duration: ";
 			std::cin >> dur;
 			playList[i].setDuration(dur);
-			
+
 			//delete track;
 		}
 	}
@@ -138,8 +83,9 @@ public:
 			{
 				if (playList[i].getName() == name)
 				{
-					std::cout << "Track name: " << playList[i].getName() << " , track date: "
-						<< std::put_time(&playList[i].getDate(), "%d/%m/%y") << " , duration: " << playList[i].getDuration() << std::endl;
+					std::cout << "Track name: " << playList[i].getName() << std::endl;
+					playList[i].getDate();
+					std::cout << "Duration: " << playList[i].getDuration() << std::endl;
 					current = playList[i];
 				}
 			}
@@ -149,12 +95,13 @@ public:
 		else if (pause)
 		{
 			std::cout << "Pause off!" << std::endl;
-			std::cout << "Track name: " << current.getName() << " , track date: "
-				<< std::put_time(&current.getDate(), "%d/%m/%y") << " , duration: " << current.getDuration() << std::endl;
-			
+			std::cout << "Track name: " << current.getName() << std::endl;
+			current.getDate();
+			std::cout << "Duration: " << current.getDuration() << std::endl;
+
 			pause = false;
 		}
-		
+
 	}
 
 	void pa()
@@ -169,9 +116,9 @@ public:
 	void ne()
 	{
 		int nextTrack = rand() % 8;
-		std::cout << "Track name: " << playList[nextTrack].getName() << " , track date: "
-			<< std::put_time(&playList[nextTrack].getDate(), "%d/%m/%y") << " , duration: " << 
-			playList[nextTrack].getDuration() << std::endl;
+		std::cout << "Track name: " << playList[nextTrack].getName() << std::endl;
+		playList[nextTrack].getDate();
+		std::cout << "Duration: " << playList[nextTrack].getDuration() << std::endl;
 		current = playList[nextTrack];
 		play = true;
 		pause = false;
@@ -186,4 +133,59 @@ public:
 			pause = false;
 		}
 	}
+
+	
 };
+
+enum operation
+{
+	play = 0,
+	pause = 1,
+	next = 2,
+	stop = 3,
+	ex = 4
+};
+
+int main()
+{
+	int op;
+	std::cout << "Audio Player." << std::endl;
+	Player player;
+	std::cout << "Loading tracks: " << std::endl;
+	player.setList();
+	while (true)
+	{
+		std::cout << "Choose the operation: ";
+		std::cin >> op;
+		if (op > 4 || op < 0)
+		{
+			std::cerr << "Error! Wrong operation! Try again!" << std::endl;
+		}
+		else
+		{
+			switch (op)
+			{
+			case operation::play:
+				player.pl();
+				break;
+			case operation::pause:
+				player.pa();
+				break;
+			case operation::next:
+				player.ne();
+				break;
+			case operation::stop:
+				player.st();
+				break;
+			case operation::ex:
+				
+				return 0;
+
+			}
+		}
+
+
+	}
+
+
+}
